ba6657fc5e253aced37360f6eccfabc121aacb93
muratdayan <muratdayan359@gmail.com>
2024-10-04 19:37:46 +0300

    Hilt and Ksp dependencies added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hiltAndroid)
+    alias(libs.plugins.kotlinAndroidKsp)
 }
 
 android {
@@ -45,4 +47,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //HILT
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
 }
