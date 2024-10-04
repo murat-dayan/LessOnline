406ec7d5c72479c9a9461c4022c7a33020b251e8
muratdayan <muratdayan359@gmail.com>
2024-10-04 19:42:09 +0300

    mvvm package structure packages added

--- app/src/main/java/com/muratdayan/lessonline/presentation/MainActivity.kt
+++ app/src/main/java/com/muratdayan/lessonline/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.muratdayan.lessonline
+package com.muratdayan.lessonline.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.muratdayan.lessonline.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
