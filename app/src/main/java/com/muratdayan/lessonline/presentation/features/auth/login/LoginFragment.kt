c57d92153806b7b50e7c414696124c0638b9fbc0
muratdayan <muratdayan359@gmail.com>
2024-10-05 17:26:16 +0300

    feat:firebase login added

--- app/src/main/java/com/muratdayan/lessonline/presentation/features/auth/login/LoginFragment.kt
+++ app/src/main/java/com/muratdayan/lessonline/presentation/features/auth/login/LoginFragment.kt
@@ -5,14 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.Navigation
 import com.muratdayan.lessonline.R
 import com.muratdayan.lessonline.databinding.FragmentLoginBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
-
+@AndroidEntryPoint
 class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+    private val loginViewModel: LoginViewModel by viewModels()
 
 
     override fun onCreateView(
@@ -25,6 +33,31 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            loginViewModel.loginState.collectLatest {loginState->
+                when(loginState){
+                    LoginState.Nothing -> {}
+                    LoginState.Loading -> {}
+                    LoginState.Success -> {
+                        //Navigation.findNavController(requireView()).navigate(R.id.action_loginFragment_to_homeFragment)
+                    }
+                    LoginState.Error -> {
+                        Toast.makeText(requireContext(), "Error", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+
+
+        binding.btnLogin.setOnClickListener {
+            val email = binding.etUsername.text.toString().trim()
+            val password = binding.etPassword.text.toString().trim()
+
+            if (email.isNotEmpty() && password.isNotEmpty()){
+                loginViewModel.login(email, password)
+            }
+        }
 
     }
 
--- app/src/main/java/com/muratdayan/lessonline/presentation/features/auth/login/LoginViewModel.kt
+++ app/src/main/java/com/muratdayan/lessonline/presentation/features/auth/login/LoginViewModel.kt
@@ -3,6 +3,8 @@ package com.muratdayan.lessonline.presentation.features.auth.login
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,7 +12,38 @@ class LoginViewModel @Inject constructor(
     private val firebaseAuth: FirebaseAuth
 ) : ViewModel() {
 
+    private val _loginState = MutableStateFlow<LoginState>(LoginState.Nothing)
+    val loginState = _loginState.asStateFlow()
 
 
+    fun login(
+        email: String,
+        password: String
+    ){
+        _loginState.value = LoginState.Loading
+
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener {task->
+                if (task.isSuccessful){
+                    task.result.user?.let {
+                        _loginState.value = LoginState.Success
+                        return@addOnCompleteListener
+                    }
+                    _loginState.value = LoginState.Error
+                }else{
+                    _loginState.value = LoginState.Error
+                }
+            }
+
+    }
+
+
+}
+
+sealed class LoginState{
+    object Nothing: LoginState()
+    object Loading: LoginState()
+    object Success: LoginState()
+    object Error: LoginState()
 
 }
