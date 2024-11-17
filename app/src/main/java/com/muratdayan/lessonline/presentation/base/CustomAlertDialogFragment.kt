48ffca99f61495cfa53fcc11afc50d1c70ab6ac0
muratdayan <muratdayan359@gmail.com>
2024-11-17 15:31:40 +0300

    style: custom alert dialog styled radius and transparent and width etc.

--- app/src/main/java/com/muratdayan/lessonline/presentation/base/CustomAlertDialogFragment.kt
+++ app/src/main/java/com/muratdayan/lessonline/presentation/base/CustomAlertDialogFragment.kt
@@ -8,22 +8,23 @@ import androidx.fragment.app.DialogFragment
 import com.muratdayan.lessonline.databinding.DialogFragmentCustomAlertDialogBinding
 
 class CustomAlertDialogFragment(
-    private val message:String,
-    private val positiveText:String,
-    private val negativeText:String,
-    private val onPositiveClick:()->Unit,
-    private val onNegativeClick:()->Unit
+    private val message: String,
+    private val positiveText: String,
+    private val negativeText: String,
+    private val onPositiveClick: () -> Unit,
+    private val onNegativeClick: () -> Unit
 ) : DialogFragment() {
 
-    private var _binding : DialogFragmentCustomAlertDialogBinding?=null
+    private var _binding: DialogFragmentCustomAlertDialogBinding? = null
     private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View{
-        _binding = DialogFragmentCustomAlertDialogBinding.inflate(inflater,container,false)
+    ): View {
+        _binding = DialogFragmentCustomAlertDialogBinding.inflate(inflater, container, false)
 
         binding.tvAlertMessage.text = message
         binding.tvPositive.text = positiveText
@@ -39,18 +40,28 @@ class CustomAlertDialogFragment(
             dismiss()
         }
 
-        return  binding.root
+        return binding.root
     }
 
     override fun onStart() {
         super.onStart()
-        dialog?.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT)
+        dialog?.window?.setBackgroundDrawableResource(android.R.color.transparent)
+
+        val params: ViewGroup.LayoutParams? = dialog?.window?.attributes
+        if (params != null) {
+            val displayMetrics = resources.displayMetrics
+            val width = (displayMetrics.widthPixels * 0.9).toInt() // Ekran genişliğinin %90'ı
+            params.width = width
+            params.height = ViewGroup.LayoutParams.WRAP_CONTENT
+            dialog?.window?.attributes = params as android.view.WindowManager.LayoutParams
+        }
     }
+
 
     override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+            super.onDestroyView()
+            _binding = null
+        }
+
+
     }
-
-
-}
