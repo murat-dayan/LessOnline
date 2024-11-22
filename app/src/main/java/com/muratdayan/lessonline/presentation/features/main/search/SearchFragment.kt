86d3365c752acac59509c7759915727b4b669d9e
muratdayan <muratdayan359@gmail.com>
2024-11-22 21:11:34 +0300

    feat: style SearchFragment edittext

--- app/src/main/java/com/muratdayan/lessonline/presentation/features/main/search/SearchFragment.kt
+++ app/src/main/java/com/muratdayan/lessonline/presentation/features/main/search/SearchFragment.kt
@@ -1,15 +1,19 @@
 package com.muratdayan.lessonline.presentation.features.main.search
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.google.android.material.internal.ViewUtils.hideKeyboard
+import com.google.android.material.textfield.TextInputLayout
 import com.muratdayan.core.presentation.BaseFragment
 import com.muratdayan.lessonline.databinding.FragmentSearchBinding
 import com.muratdayan.lessonline.presentation.adapter.BasicPostListAdapter
@@ -35,8 +39,11 @@ class SearchFragment : BaseFragment() {
         return binding.root
     }
 
+    @SuppressLint("RestrictedApi")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.tilSearch.endIconMode = TextInputLayout.END_ICON_NONE
 
         basicPostListAdapter = BasicPostListAdapter(){postId->
             val action = SearchFragmentDirections.actionSearchFragmentToPostDetailFragment(postId)
@@ -72,14 +79,47 @@ class SearchFragment : BaseFragment() {
 
             override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 val query = s.toString()
+                if (s != null) {
+                    if(s.isNotBlank()){
+                        binding.tilSearch.endIconMode = TextInputLayout.END_ICON_CUSTOM
+                    }
+                }
                 searchViewModel.searchPosts(query)
             }
 
             override fun afterTextChanged(p0: Editable?) {
+                binding.tilSearch.setEndIconOnClickListener {
+                    binding.etSearch.text?.clear()
+                    binding.tilSearch.endIconMode = TextInputLayout.END_ICON_NONE
+
+                }
             }
         })
 
+        binding.etSearch.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                val query = binding.etSearch.text.toString().trim() // Kullanıcıdan gelen metni temizle
+                if (query.isNotBlank()) {
+                    searchViewModel.searchPosts(query) // Arama işlemini başlat
+                    binding.tilSearch.endIconMode = TextInputLayout.END_ICON_NONE
+                    hideKeyboard(requireView()) // Klavyeyi kapat
+                    true // Olay işlenmiş olarak işaretle
+                } else {
+                    binding.tilSearch.endIconMode = TextInputLayout.END_ICON_NONE
+                    hideKeyboard(requireView()) // Klavyeyi kapat
+                    false // Olayı işlenmemiş bırak
+                }
+            } else {
+                false // Diğer action'ları işleme
+            }
+        }
+
         searchViewModel.fetchPosts()
+
+        binding.tilSearch.setEndIconOnClickListener {
+            binding.etSearch.text?.clear()
+            binding.tilSearch.endIconMode = TextInputLayout.END_ICON_NONE
+        }
     }
 
     override fun onDestroy() {
